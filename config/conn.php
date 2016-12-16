f49fd25c70de4a511ff0b3379a4f6c3f79f75ea2
Andrew Shell <andrew@andrewshell.org>
2016-12-16 10:21:38 -0600

    Updating conn.php to support sqlite

--- config/conn.php
+++ config/conn.php
@@ -3,8 +3,19 @@ $phinx = include(__DIR__ . '/phinx.php');
 $default = $phinx['environments']['default_database'];
 $env = $phinx['environments'][$default];
 
-return [
-    sprintf('%s:dbname=%s;host=%s', $env['adapter'], $env['name'], $env['host']),
-    $env['user'],
-    $env['pass']
-];
+if (0 === strcmp('sqlite', $env['adapter'])) {
+    if (empty($env['name']) && true === $env['memory']) {
+        $env['name'] = ':memory:';
+    }
+    return [
+        sprintf('%s:%s', $env['adapter'], $env['name']),
+        null,
+        null
+    ];
+} else {
+    return [
+        sprintf('%s:dbname=%s;host=%s', $env['adapter'], $env['name'], $env['host']),
+        $env['user'],
+        $env['pass']
+    ];
+}
